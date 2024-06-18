c9686e71c284c7d6f1cf566da4083889f9102e49
Matheus Alves <malves224@gmail.com>
2024-06-18 16:55:55 -0300

    use variable VUE_APP_BACKEND_URL

--- src/cable.js
+++ src/cable.js
@@ -1,6 +1,6 @@
 // src/cable.js
 import { createConsumer } from "@rails/actioncable";
 
-const cable = createConsumer(`ws://159.65.236.207:3000/cable`);
+const cable = createConsumer(`ws://${process.env.VUE_APP_BACKEND_URL}/cable`);
 
 export default cable;
--- src/services/robotService.js
+++ src/services/robotService.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const BACKEND_URL = `http://159.65.236.207:3000`
+const BACKEND_URL = `http://${process.env.VUE_APP_BACKEND_URL}`;
 const apiUrl = `${BACKEND_URL}/robots`;
 
 const robotService = {
   getAllRobots: async () => {
-    try {
-      const response = await axios.get(apiUrl);
+      const response = await axios.get(apiUrl, {
+        headers: {
+          'Access-Control-Allow-Origin': 'http://159.65.236.207:8080',
+        }
+      });
       return response.data;
-    } catch (error) {
-      console.error('Error fetching robots:', error);
-      throw error;
-    }
   }
 };
 
